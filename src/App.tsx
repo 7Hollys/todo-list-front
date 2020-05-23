656759e78645e216b42d8b0940ed7630f74cc1f1
박두영 <parkduyoung@bagduyeong-ui-MacBookPro.local>
2020-05-23 17:56:06 +0900

    install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.scss";
+import React from "react"
+import "./App.scss"
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
