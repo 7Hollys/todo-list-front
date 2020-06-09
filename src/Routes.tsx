1aa74ff5d4f922decf3cc879039edd3a25bc87e9
syalot005006 <syalottdeveloper@gmail.com>
2020-06-09 19:22:22 +0900

    Resolve #7

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,17 @@
 import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom"
 
 import SignIn from "./pages/signIn/SignIn"
+import TodoList from "./pages/todoList/TodoList"
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/" component={SignIn} />
-      <Route path="/signIn" component={SignIn} />
+      <Switch>
+        <Route path="/sign_in" component={SignIn} />
+        <Route path="/" component={TodoList} />
+        <Redirect to="/" />
+      </Switch>
     </Router>
   )
 }
