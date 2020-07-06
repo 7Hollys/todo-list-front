d2fdc6d1bda554af84e661d2e54a834938f95600
syalot005006 <syalottdeveloper@gmail.com>
2020-07-07 00:17:41 +0900

    Resolve #13

--- src/pages/auth/token/Token.tsx
+++ src/pages/auth/token/Token.tsx
@@ -1,37 +1,49 @@
-import React from "react"
+import React, { useEffect, useLayoutEffect } from "react"
 import { connect } from "react-redux"
 import axios from "axios"
 import { signIn } from "modules/user"
 import { useHistory } from "react-router-dom"
+import { IStore } from "modules"
 
-interface Props {
+interface IProps {
+  token: string
   signIn: Function
 }
 
-const Token = ({ signIn }: Props) => {
-  const params = new URL(document.location.href).searchParams
-  const token = params.get("token")
+const Token = ({ token, signIn }: IProps) => {
   const history = useHistory()
 
-  axios
-    .get(`${process.env.REACT_APP_API_URL}/api/users/me`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "content-type": "application/json",
-      },
-    })
-    .then(function (response) {
-      signIn({
-        email: response.data.email,
-        name: response.data.name,
-        profileImage: response.data.profileImage,
-        token: token,
+  useLayoutEffect(() => {
+    const params = new URL(document.location.href).searchParams
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/users/me`, {
+        headers: {
+          Authorization: `Bearer ${params.get("token")}`,
+          "content-type": "application/json",
+        },
       })
+      .then(function (response) {
+        signIn({
+          email: response.data.email,
+          name: response.data.name,
+          profileImage: response.data.profileImage,
+          token: params.get("token"),
+        })
+      })
+  }, [signIn])
 
+  useEffect(() => {
+    if (token) {
       history.replace("/todo_list")
-    })
+    }
+  }, [history, token])
 
   return <div></div>
 }
 
-export default connect(null, { signIn })(Token)
+export default connect(
+  (store: IStore) => ({
+    token: store.user.token,
+  }),
+  { signIn }
+)(Token)
